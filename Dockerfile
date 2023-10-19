1299ecef87def28ae7180cb58bcbffae9368ebb2
esataydin <esat61_2001@hotmail.com>
2023-10-19 18:11:36 +0200

    docker run works

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,25 @@ FROM mcr.microsoft.com/dotnet/sdk:7.0 as build-env
 WORKDIR /app
 EXPOSE 80
 EXPOSE 443
-
-COPY *.csproj ./
-RUN dotnet restore
-
 COPY . ./
-RUN dotnet publish -c Release -o /app/out
-
+RUN dotnet restore
+COPY . ./
+RUN dotnet publish -c Release -o out
 FROM mcr.microsoft.com/dotnet/sdk:7.0
-WORKDIR / app
+WORKDIR /app
 COPY --from=build-env /app/out .
-ENTRYPOINT [ "dotnet", "ci-cd-api.dll" ]
+ENTRYPOINT ["dotnet", "ci-cd-api.dll"]
+
+#docker build -t kubapi . #run this command again if you want to update the container with your latest code
+#Use a specific port that runs the application in the container
+#docker run -p 8082:80 -e ASPNETCORE_URLS=http://+:80 kubapi
+#docker build . -t esataydin/kubapi:v1
+#docker push esataydin/kubapi:v1
+#kubectl get all
+#kubectl apply -f deployment.yaml # goes into kubernetes ci and apply then deployment file
+#kubectl delete -f deployment.yaml
+
+#dcoker ps # see running docker images
+#docker logs <container-id>
+#docker stop 7a5aac75fb2d  # Stop the current container
+#docker run -d -p 8080:5000 my-cicd-app:v1  # Create a new container with port mapping
--- Program.cs
+++ Program.cs
@@ -1,25 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using System;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace Application
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var builder = WebApplication.CreateBuilder(args);
+        
 
-app.UseHttpsRedirection();
+        // Add services to the container.
 
-app.UseAuthorization();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-app.MapControllers();
+        var app = builder.Build();
 
-app.Run();
+        // Configure the HTTP request pipeline.
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        // app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+        }
+      }
+    }
